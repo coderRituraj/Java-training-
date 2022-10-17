e28ddd12977c291fa78a3e0234acc391538e2fd6
coderRituraj <110223342+coderRituraj@users.noreply.github.com>
2022-10-17 11:47:54 +0530

    Task

---
+++ Date-12-10/Demo_Spring_Boot_Project/src/main/java/com/bhavna/ApplicationSpring.java
@@ -0,0 +1,18 @@
+package com.bhavna;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationSpring {
+	
+
+
+	   public static void main(String[] args) {
+	        SpringApplication.run(ApplicationSpring.class,args);
+	        System.out.println("Project Sucessful");
+	    }
+
+
+
+	
+}
